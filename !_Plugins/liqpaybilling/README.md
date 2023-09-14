3f856aaebb2f54fd90c4e5b7543052aefaf19488
webitproff <webitproff@gmail.com>
2023-09-14 16:39:11 +0300

    Add files via upload

---
+++ !_Plugins/liqpaybilling/README.md
@@ -0,0 +1,10 @@
+cot-liqpay
+==========
+
+Платежный плагин LiqPay для платежного модуля Payments на Cotonti
+
+Описание и инструкция по установке: http://cmsworks.ru/catalog/plugins/liqpal
+
+Разработчик: Булат Юсупов support@cmsworks.ru
+
+Copyright CMSWorks Team 2014
---
+++ !_Plugins/logincheck/lang/logincheck.ru.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['cfg_invalidnames'] = array('Запрещенные логины', 'через запятую');
+
+$L['logincheck_error_invalidchars'] = 'Логин содержит недопустимые символы';
+$L['logincheck_error_invalidname'] = 'Пожалуйста, укажите другой логин';
