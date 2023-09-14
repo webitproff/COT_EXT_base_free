993aeb4e35928dbb618213b066d3879af6a59c10
webitproff <webitproff@gmail.com>
2023-09-14 12:48:55 +0300

    Add files via upload

---
+++ !_Plugins/molliebilling/inc/vendor/guzzlehttp/guzzle/src/Exception/TooManyRedirectsException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace _PhpScoper5d8cb17438769\GuzzleHttp\Exception;
+
+class TooManyRedirectsException extends \_PhpScoper5d8cb17438769\GuzzleHttp\Exception\RequestException
+{
+}
---
+++ !_Plugins/molliebilling/inc/vendor/guzzlehttp/guzzle/src/Exception/TransferException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace _PhpScoper5d8cb17438769\GuzzleHttp\Exception;
+
+class TransferException extends \RuntimeException implements \_PhpScoper5d8cb17438769\GuzzleHttp\Exception\GuzzleException
+{
+}
